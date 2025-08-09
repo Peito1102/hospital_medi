e1fa588c2fb342496926908bf45e804339ac77cd
Peito1102 <renzitiwin@gmail.com>
2025-08-08 22:20:40 -0500

    first commit

---
+++ src/main/java/com/vasquez/hospital_medi/HospitalMediApplication.java
@@ -0,0 +1,13 @@
+package com.vasquez.hospital_medi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalMediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasquez/hospital_medi/HospitalMediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasquez.hospital_medi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
