645e338d85624f2fe6bd21a3ac4b58b839ddc990
Peito1102 <renzovasquezch@hotmail.com>
2025-08-19 03:59:47 -0500

    adicionando repository

---
+++ src/main/java/com/vasquez/hospital_medi/repositories/CityRepository.java
@@ -0,0 +1,9 @@
+package com.vasquez.hospital_medi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vasquez.hospital_medi.entities.City;
+
+public interface CityRepository extends JpaRepository<City, Long>{
+
+}
